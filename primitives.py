d060894bdf7444a6a2c9db74caae2fa5b182fbe2
graeme <graeme@lutralutra.com>
2020-02-10 03:45:28 +0000

    first examples

---
+++ primitives.py
@@ -0,0 +1,105 @@
+import numpy as np
+import pygame
+
+class Vec3D(object):
+    """A vertex in 3D space"""
+
+    def __init__(self, pos=np.array([0.0, 0.0, 0.0])):
+        self.pos = np.array(pos)
+
+    @property
+    def x(self):
+        return self.pos[0]
+
+    @property
+    def y(self):
+        return self.pos[1]
+
+    @property
+    def z(self):
+        return self.pos[2]
+
+    def translate(self, vector=[0.0, 0.0, 0.0]):
+        return Vec3D([self.x + vector[0],
+                      self.y + vector[1],
+                      self.z + vector[2]])
+
+    def transform(self, m):
+        """transform vertex using given matrix"""
+        '''
+        # explicit version
+        new_x = self.x * m[0][0] + self.y * m[1][0] + self.z * m[2][0] + m[3][0]
+        new_y = self.x * m[0][1] + self.y * m[1][1] + self.z * m[2][1] + m[3][1]
+        new_z = self.x * m[0][2] + self.y * m[1][2] + self.z * m[2][2] + m[3][2]
+        w = self.x * m[0][3] + self.y * m[1][3] + self.z * m[2][3] + m[3][3]
+
+        if w != 0.0:
+            new_x /= w
+            new_y /= w
+            new_z /= w
+
+        return Vec3D([new_x, new_y, new_z])
+        '''
+        new_vec = np.dot(list(self.pos)+[1], m)
+        if new_vec[3] != 0.0:
+            new_vec /= new_vec[3]
+        return Vec3D(new_vec[:3])
+
+
+    def scale(self, width, height):
+        """scale vertex to given screen height and width"""
+        return Vec3D([(self.x + 1.0) * (0.5 * width),
+                      (self.y + 1.0) * (0.5 * height),
+                      self.z])
+
+class Triangle(object):
+    """A triangle in 3D space defined as a triplet of vertices"""
+
+    def __init__(self, vec1, vec2, vec3):
+        self.vecs = [vec1, vec2, vec3]
+
+    @property
+    def vec1(self):
+        return self.vecs[0]
+
+    @property
+    def vec2(self):
+        return self.vecs[1]
+
+    @property
+    def vec3(self):
+        return self.vecs[2]
+
+    def translate(self, vector=[0.0, 0.0, 0.0]):
+        """translate triangle by given values"""
+        return Triangle(self.vec1.translate(vector),
+                        self.vec2.translate(vector),
+                        self.vec3.translate(vector))
+
+    def transform(self, matrix):
+        """transform triangle using given matrix"""
+        return Triangle(self.vec1.transform(matrix),
+                        self.vec2.transform(matrix),
+                        self.vec3.transform(matrix))
+
+    def scale(self, width, height):
+        """scale triangle to given screen height and width"""
+        return Triangle(self.vec1.scale(width, height),
+                        self.vec2.scale(width, height),
+                        self.vec3.scale(width, height))
+
+    def draw(self, screen, colour):
+        """draw triangle on pygame screen object"""
+        pygame.draw.line(screen, colour, [self.vec1.x, self.vec1.y], [self.vec2.x, self.vec2.y])
+        pygame.draw.line(screen, colour, [self.vec2.x, self.vec2.y], [self.vec3.x, self.vec3.y])
+        pygame.draw.line(screen, colour, [self.vec3.x, self.vec3.y], [self.vec1.x, self.vec1.y])
+
+class Mesh(object):
+    """A mesh in 3D space defined as a collection of triangles"""
+
+    def __init__(self, tris=[]):
+        self.tris = []
+        for tri in tris:
+            self.tris.append(Triangle(Vec3D(tri[0:3]),
+                                      Vec3D(tri[3:6]),
+                                      Vec3D(tri[6:9])))
---
+++ spinning_cube.py
@@ -0,0 +1,129 @@
+import pygame
+import numpy as np
+import time
+from primitives import Triangle, Mesh
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 460
+WINDOW_TITLE = 'Spinning Cube Demo'
+MAX_FRAMERATE = 0
+
+white = (255,255,255)
+black = (0,0,0)
+red = (255,0,0)
+
+def main():
+
+    # initialize the pygame module
+    pygame.init()
+    # load and set the logo
+    logo = pygame.image.load("img/logo32x32.png")
+    pygame.display.set_icon(logo)
+    pygame.display.set_caption(WINDOW_TITLE)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # define a variable to control the main loop
+    running = True
+
+    # projection matrix
+    fNear = 0.1
+    fFar = 1000.0
+    fFov = 90.0
+    fFovRad = 1.0 / np.tan(fFov * 0.5 / 180.0 * np.pi)
+    fAspectRatio = float(SCREEN_HEIGHT) / float(SCREEN_WIDTH)
+
+    matProj = np.zeros((4,4))
+    matProj[0][0] = fAspectRatio * fFovRad
+    matProj[1][1] = fFovRad
+    matProj[2][2] = fFar / (fFar - fNear)
+    matProj[3][2] = (-fFar * fNear) / (fFar - fNear)
+    matProj[2][3] = 1.0
+
+    # create cube Mesh
+    cubeMesh = Mesh([
+        # SOUTH
+		[0.0, 0.0, 0.0,    0.0, 1.0, 0.0,    1.0, 1.0, 0.0],
+		[0.0, 0.0, 0.0,    1.0, 1.0, 0.0,    1.0, 0.0, 0.0],
+
+		# EAST
+		[1.0, 0.0, 0.0,    1.0, 1.0, 0.0,    1.0, 1.0, 1.0],
+		[1.0, 0.0, 0.0,    1.0, 1.0, 1.0,    1.0, 0.0, 1.0],
+
+		# NORTH
+		[1.0, 0.0, 1.0,    1.0, 1.0, 1.0,    0.0, 1.0, 1.0],
+		[1.0, 0.0, 1.0,    0.0, 1.0, 1.0,    0.0, 0.0, 1.0],
+
+		# WEST
+		[0.0, 0.0, 1.0,    0.0, 1.0, 1.0,    0.0, 1.0, 0.0],
+		[0.0, 0.0, 1.0,    0.0, 1.0, 0.0,    0.0, 0.0, 0.0],
+
+		# TOP
+		[0.0, 1.0, 0.0,    0.0, 1.0, 1.0,    1.0, 1.0, 1.0],
+		[0.0, 1.0, 0.0,    1.0, 1.0, 1.0,    1.0, 1.0, 0.0],
+
+		# BOTTOM
+		[1.0, 0.0, 1.0,    0.0, 0.0, 1.0,    0.0, 0.0, 0.0],
+		[1.0, 0.0, 1.0,    0.0, 0.0, 0.0,    1.0, 0.0, 0.0],])
+
+    # start timing for rotation calculations
+    last_update = time.time()
+    fTheta = 0.0
+
+    # main loop
+    while running:
+        # event handling, gets all event from the event queue
+        for event in pygame.event.get():
+            # only do something if the event is of type QUIT
+            if event.type == pygame.QUIT:
+                # change the value to False, to exit the main loop
+                running = False
+
+        screen.fill(black)
+        for tri in cubeMesh.tris:
+
+            # get time elapsed since last update
+            delta_t = time.time() - last_update
+            fTheta += delta_t
+
+            # calculate Z-rotation matrix for given time elapsed
+            matRotZ = np.zeros((4,4))
+            matRotZ[0][0] = np.cos(fTheta)
+            matRotZ[0][1] = np.sin(fTheta)
+            matRotZ[1][0] = -1.0 * np.sin(fTheta)
+            matRotZ[1][1] = np.cos(fTheta)
+            matRotZ[2][2] = 1.0
+            matRotZ[3][3] = 1.0
+
+            # calculate X-rotation matrix for given time elapsed
+            matRotX = np.zeros((4,4))
+            matRotX[0][0] = 1
+            matRotX[1][1] = np.cos(fTheta * 0.5)
+            matRotX[1][2] = np.sin(fTheta * 0.5)
+            matRotX[2][1] = -1 * np.sin(fTheta * 0.5)
+            matRotX[2][2] = np.cos(fTheta * 0.5)
+            matRotX[3][3] = 1
+
+            # rotate in Z axis
+            triRotatedZ = tri.transform(matRotZ)
+            triRotatedZX = triRotatedZ.transform(matRotX)
+
+            # translate in z axis
+            triTranslated = triRotatedZX.translate([0.0, 0.0, 3.0])
+
+            # project from 3D --> 2D
+            triProjected = triTranslated.transform(matProj)
+
+            # scale into view
+            triScaled = triProjected.scale(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+            # draw on screen
+            triScaled.draw(screen, white)
+
+        pygame.display.flip()
+        last_update = time.time()
+
+# run the main function only if this module is executed as the main script
+# (if you import this as a module then nothing is executed)
+if __name__=="__main__":
+    # call the main function
+    main()
